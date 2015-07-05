7cb1cc0a555c34d0fb52c0aedd9521eb92b4e349
vikingeff <mail.leger@gmail.com>
2015-07-05 17:04:36 +0200

    expert

---
+++ main.ml
@@ -0,0 +1,44 @@
+(* ************************************************************************** *)
+(*                                                                            *)
+(*                                                        :::      ::::::::   *)
+(*   jokes.ml                                           :+:      :+:    :+:   *)
+(*                                                    +:+ +:+         +:+     *)
+(*   By: gleger <marvin@42.fr>                      +#+  +:+       +#+        *)
+(*                                                +#+#+#+#+#+   +#+           *)
+(*   Created: 2015/06/22 16:11:02 by gleger            #+#    #+#             *)
+(*   Updated: 2015/06/22 16:11:04 by gleger           ###   ########.fr       *)
+(*                                                                            *)
+(* ************************************************************************** *)
+
+let create_lst file = 
+	let input = open_in file in 
+	let lst = [] in
+	try
+		while true do
+			(input_line input)::lst;
+		done;
+	with
+		| End_of_file -> close_in input
+		| _ -> print_endline "There was an error, just don't know which one."
+	arrayz
+
+let main file =
+	try
+		let jokez = create_lst file in
+		Random.self_init ();
+		let index = Random.int nb_jokes in
+		print_endline (List.nth jokes lst)
+	with
+		| End_of_file -> print_endline "Seriously ?? Empty file, well that means no joke then !!"
+		| Failure err -> print_endline "Joke file doesn't seems well formated."
+		| _ -> print_endline "There was an error, just don't know which one."
+
+	(* ************************************************************************** *)
+let () =
+	let argv = Array.to_list Sys.argv in
+	if (List.length argv) <= 1 then
+		print_endline "Error: Need the name of joke file !"
+	else if (List.length argv) > 2 then
+		print_endline "Error: Need less args."
+	else 
+		main (List.nth argv 1)
